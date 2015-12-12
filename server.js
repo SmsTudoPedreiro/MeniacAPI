8cad1cf7214cf051ed3a406f9157064b46f980c7
CristinaSilvaEng <cristina.silva@openmailbox.org>
2015-12-12 12:07:44 -0200

    Muda mongoose por mongojs

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
 
+var mongojs = require('mongojs');
+var database_url = process.env.MONGOLAB_URI || 'mongodb://localhost/demo_database';
+var collections = ['criminalminds'];
+var db = mongojs(database_url, collections, {authMechanism: 'ScramSHA1'});
+
 app.set('port', (process.env.PORT || 3000));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function (request, response) {
+
+  db.criminalminds.find(function (err, docs) {
+    console.log(docs);
+    response.json(docs);
+  });
 });
 
 app.listen(app.get('port'), function () {
